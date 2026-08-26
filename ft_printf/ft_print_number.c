89f5c7bcf9ed63d8c75ac4f7ff361ce2af738031
han <bayramincel55@gmail.com>
2026-08-26 20:24:42 +0300

    :3

---
+++ ft_printf/ft_print_number.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printfnumber.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: haincel <haincel@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/26 16:21:24 by haincel           #+#    #+#             */
+/*   Updated: 2026/08/26 19:33:51 by haincel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putnbr_fd(int nb, int fd)
+{
+	int	len;
+
+	len = 0;
+	if (nb == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return (11);
+	}
+	if (nb < 0)
+	{
+		len += ft_putchar_fd('-', fd);
+		nb = -nb;
+	}
+	if (nb >= 10)
+		len += ft_putnbr_fd(nb / 10, fd);
+	len += ft_putchar_fd((nb % 10) + '0', fd);
+	return (len);
+}
+
+int	ft_putnbr_unsigned_fd(unsigned int nb, int fd)
+{
+	int	len;
+
+	len = 0;
+	if (nb >= 10)
+		len += ft_putnbr_unsigned_fd(nb / 10, fd);
+	len += ft_putchar_fd((nb % 10) + '0', fd);
+	return (len);
+}
--- ft_printf/ft_print_string.c
+++ ft_printf/ft_print_string.c
@@ -1,27 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_print_string.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: haincel <haincel@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/25 15:17:37 by haincel           #+#    #+#             */
-/*   Updated: 2026/08/26 19:33:22 by haincel          ###   ########.fr       */
+/*   Created: 2026/08/26 16:21:26 by haincel           #+#    #+#             */
+/*   Updated: 2026/08/26 20:09:02 by haincel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <stdarg.h>
-# include <unistd.h>
+int	ft_putchar_fd(char c, int fd)
+{
+	return (write(fd, &c, 1));
+}
 
-int		ft_printf(const char *type, ...);
-int		ft_putchar_fd(char c, int fd);
-int		ft_putnbr_fd(int nb, int fd);
-int		ft_putstr_fd(char *s, int fd);
-int		ft_putptr_fd(unsigned long nb, int fd);
-int		ft_puthex_fd(unsigned int nb, int fd, char *base);
-int		ft_putnbr_unsigned_fd(unsigned int nb, int fd);
+int	ft_putstr_fd(char *s, int fd)
+{
+	int	len;
 
-#endif
+	if (!s)
+		return (ft_putstr_fd("(null)", fd));
+	len = 0;
+	while (s[len])
+	{
+		ft_putchar_fd(s[len], fd);
+		len++;
+	}
+	return (len);
+}
